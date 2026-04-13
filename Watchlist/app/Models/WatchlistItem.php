0c1d26722b38e3d817d47438269b25714be20c08
Jarne <jarne.kampshof@student.graafschapcollege.nl>
2026-04-13 18:14:09 +0200

    problems gefixed

--- Watchlist/app/Models/WatchlistItem.php
+++ Watchlist/app/Models/WatchlistItem.php
@@ -4,6 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $type
+ * @property string $status
+ * @property int $year
+ * @property int|null $rating
+ * @property string|null $image_path
+ */
 class WatchlistItem extends Model
 {
     protected $fillable = [
--- Watchlist/app/Models/gebruikersgegevens.php
+++ Watchlist/app/Models/gebruikersgegevens.php
@@ -4,6 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $voornaam
+ * @property string $achternaam
+ * @property string $email
+ * @property string|null $telefoonnummer
+ * @property string $wachtwoord
+ */
 class gebruikersgegevens extends Model
 {
     protected $fillable = [
